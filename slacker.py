6d01ddb1f5d2631d35ce65bbc30327cec3526991
HGee <harrison.gee1@live.co.uk>
2018-04-21 13:54:48 +1000

    Hello Daddies
    yote

--- slacker.py
+++ slacker.py
@@ -6,7 +6,23 @@ class coolmathsgames(sublime_plugin.TextCommand):
 	def run(self, edit):
 		contents = self.view.substr(sublime.Region(0, self.view.size()))
 		self.view.erase(edit, sublime.Region(0, self.view.size()))
-		self.view.insert(edit, 0, contents + " memes")
+		file = open("contents.txt","w") 
+		file.write(contents) 
+
+		self.view.insert(edit, 0, "i really enjoy memes")
+
+class uncoolmathsgames(sublime_plugin.TextCommand):
+	"""docstring for ClassName"""
+	def run(self, edit):
+		file = open("contents.txt", "r") 
+		contents = file.read() 
+		self.view.erase(edit, sublime.Region(0, self.view.size()))
+		self.view.insert(edit, 0, contents)
+		
+
+
+
+		
 
 
 
