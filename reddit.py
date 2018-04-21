3b1be9da518a36fae77059575287b962218ea47c
Matthew Swan <mattswan@live.com>
2018-04-21 12:32:13 +1000

    added ascii_image.py

--- reddit.py
+++ reddit.py
@@ -8,11 +8,49 @@ def connect_to_reddit():
     @return
     reddit: le reddoot
     """
+    print('Connecting...')
     reddit = praw.Reddit(client_id = 'Ngjy2b_Gjedvew',
                          client_secret = 'kUbMmIc3KQ92CvcjVpHZbR647Nc',
                          username = 'teamBhackathon',
                          password = 'memes2017',
                          user_agent = 'happysubs app')
     
-    
+    print('All good mates')
     return reddit
+
+
+def get_posts_names(reddit, sub):
+    """
+    Get the names of the posts in the specified subreddit
+    reddit: reddit object
+    sub: subreddit name
+    """
+    titles = []
+    source = reddit.subreddit(sub)
+    for submission in source.hot(limit=10):
+        titles.append(submission.title)
+    
+    return titles
+
+def get_posts_urls(reddit, sub):
+    """
+    Get the names of the posts in the specified subreddit
+    reddit: reddit object
+    sub: subreddit name
+    """
+    urls = []
+    source = reddit.subreddit(sub)
+    for submission in source.hot(limit=10):
+        urls.append(submission.url)
+    
+    return urls
+
+reddit = connect_to_reddit()
+
+sub = 'memes'
+
+titles = get_posts_names(reddit, sub)
+urls = get_posts_urls(reddit, sub)
+print(titles)
+print(urls)
+    
